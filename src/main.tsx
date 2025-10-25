499073718ba4ab4d729d852c6d659c66246f1f6b
AltaciosTheDev <enzo.altamirano.dev@gmail.com>
2025-10-25 12:35:53 -0600

    small fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./component/App.tsx";
+import App from "./components/App.tsx";
 import TasksProvider from "./contexts/TasksProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
