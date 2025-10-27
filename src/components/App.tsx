3611764293e58dce093f65d643916492c7a86378
AltaciosTheDev <enzo.altamirano.dev@gmail.com>
2025-10-26 18:46:46 -0600

    fixed Auth component import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
-import Auth from "../pages/auth";
+import Auth from "../pages/Auth";
 
 function App() {
   return (
